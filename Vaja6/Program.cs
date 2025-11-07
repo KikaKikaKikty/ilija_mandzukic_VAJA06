11bf7dcf5cbdbc148f62c35ce09c712cb73c2553
Ilija MANDŽUKIĆ <ilija.mandzukic.20602@dijak.sc-celje.si>
2025-11-07 09:36:50 +0100

    public v private

--- Vaja6/Program.cs
+++ Vaja6/Program.cs
@@ -10,10 +10,10 @@ namespace vaja6 {
 	// Se en komentar
     class Igralec {
 		
-        public int id;
-        public string u_ime;
-        public string u_geslo;
-        public static int stIg = 1;
+        private int id;
+        private string u_ime;
+        private string u_geslo;
+        private static int stIg = 1;
 //nastavi neke vrednosti
         public Igralec() {
             u_ime = "";
